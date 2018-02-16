86184c897e21aae05104d556f8c72b7c48e31ca0
dingjinlin <ding.jl@30raycom.com>
2018-02-16 20:42:19 +0800

    合并

--- src/main/java/com/dingjinllin/study/MyUI.java
+++ src/main/java/com/dingjinllin/study/MyUI.java
@@ -6,11 +6,9 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+
+import com.dingjinllin.study.binglichufang.xiyichufang.Xiyichufang;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -24,22 +22,24 @@ public class MyUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        
+        /*final VerticalLayout layout = new VerticalLayout();
+
         final TextField name = new TextField();
         name.setCaption("Type your name here:");
 
         Button button = new Button("Click Me");
         button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
+            layout.addComponent(new Label("Thanks " + name.getValue()
                     + ", it works!"));
         });
-        
+
         layout.addComponents(name, button);
         layout.setMargin(true);
         layout.setSpacing(true);
-        
-        setContent(layout);
+
+        setContent(layout);*/
+        Xiyichufang xycf = new Xiyichufang();
+        setContent(xycf);
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
